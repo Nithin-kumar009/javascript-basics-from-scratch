6969e3aea830b59f71e662f1887e0dc6046abf50
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-05-30 20:17:38 +0530

    added comparision operators

--- operators in js/comparision_operators.js
+++ operators in js/comparision_operators.js
@@ -1,1 +1,15 @@
-//comparision operators
+//comparision operators - these operators comapares two variables and give output as true or false
+let a=5;
+let b=3;
+console.log("a==b",a==b); //false
+console.log("a!=b",a!=b);//true
+console.log("a<b",a<b);//false
+console.log("a<=b",a<=b);//false
+console.log("a>b",a>b);//true
+console.log("a>=b",a>=b);//true
+
+//these below operators compares both variables and datatypes of the variables
+//can give strong and strict out put
+console.log("a===b",a===b);
+console.log("a!==b",a!==b);
+
