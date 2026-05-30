fbe6524a2c10346f70636a63fc959c0804f66723
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-05-30 21:50:27 +0530

    added conditional statements

---
+++ conditional statements/if_statement.js
@@ -0,0 +1,14 @@
+// if statement
+//if the given condition is true then prints the value
+//otherwise it ignores the if block
+let age=18;
+if(age>=18){
+    console.log("you can vote");
+
+}
+if(age<18){
+    console.log("you cannot vote");
+}
+/* here age is equal to 18 so the first condition is true, so, it shows the output 
+that "you can vote".*/
+
---
+++ conditional statements/ifelse_statement.js
@@ -0,0 +1,12 @@
+// if else makes the user to write the code easily
+// instead of using if conditon again and again. we use else
+//gives output only when if condition is true , otherwise it prints else condition
+
+let age=16;
+if(age>=18){
+    console.log(" you can vote");
+
+}
+else{
+    console.log("you cannot vote");
+}
