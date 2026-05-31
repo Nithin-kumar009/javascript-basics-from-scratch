e3bd1cd1bd194b83a380b5ccbd1f276107df5730
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-05-31 10:46:21 +0530

    added do while loop

---
+++ loops and strings/dowhile.js
@@ -0,0 +1,13 @@
+/*let i=1;
+do{
+    console.log("i=",i);
+    i++;
+}while(i<=5);   */
+
+//this prints i = 1 to 5
+
+let i=10;
+do{
+    console.log("i=",i);
+    i++;
+}while(i>=20); //this prints i=10 because the condition is wrong
