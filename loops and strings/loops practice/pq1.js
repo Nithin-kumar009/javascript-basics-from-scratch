4dbee7f6ffee1044250635c4daa5cf065743902b
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-05-31 11:18:28 +0530

    added practice question

---
+++ loops and strings/loops practice/pq1.js
@@ -0,0 +1,16 @@
+// print all even numbers from 0 to 100.
+
+/*for(let num=0;num<=100;num++){
+    if(num%2===0){
+        console.log("num=",num);
+    }
+}*/
+
+//print all odd numbers from 0 to 100
+
+for(let num=0;num<=100;num++){
+    if(num%2!==0){
+        console.log("add numbers=",num);
+    }
+    
+}
