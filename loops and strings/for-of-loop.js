c7fd4b956e6e76356220c793243178e0587582b6
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-05-31 10:57:02 +0530

    added for-of loop

---
+++ loops and strings/for-of-loop.js
@@ -0,0 +1,8 @@
+//for-of loop - used to iterate chrarecters and conts the characters in a string
+let str="NithinKumarYadav";
+let size=0;
+for(let i of str){
+    console.log("i=",i);
+    size++;
+}
+console.log("srting size is",size);
