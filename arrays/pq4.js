92165fbcbedc0a10b2bc13c2f1d3a6d330848efc
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-01 09:40:03 +0530

    added array crud practices

---
+++ arrays/pq4.js
@@ -0,0 +1,14 @@
+let names=["rahul", "sonia","priys","modi" ];
+//read
+console.log(names);
+//update elememts - indexing
+console.log(names[2]);
+console.log(names[1]);
+console.log(names[7]);
+
+//update array
+
+names[1]="sonia gandhi";
+//delete element
+names.pop()
+console.log(names);
