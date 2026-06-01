95de6fd825da304688e25f0a5376b3fc38bba469
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-01 09:21:50 +0530

    added array practices

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,3 +1,3 @@
 // array - array is a collection of items
 let numbers=[10, 20, 40, 30,50];
-console.log(numbers)
+console.log(numbers);
---
+++ arrays/pq13.js
@@ -0,0 +1,6 @@
+let enames=["rg", "sg", "pg", "modi"];
+let eids=[101, 102,102,104, 105];
+
+console.log(enames);
+console.log(eids);
+document.writeln(enames);
