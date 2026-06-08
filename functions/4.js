292845f29d9341d1831e32216738834b6e8c9fe5
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-08 09:58:44 +0530

    modified 4.js

--- functions/4.js
+++ functions/4.js
@@ -1,7 +1,7 @@
 function add(a,b){
     let c=1;
-   return a+b+c;
-    console.log("peddi")
+   return a+b+c;  //here it returns to the next block
+    console.log("peddi") // un reachable statement
 }
 let r1=add(10,20)
 console.log(r1)
