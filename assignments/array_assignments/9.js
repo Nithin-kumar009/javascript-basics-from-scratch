551896205e4d101d10e21e675ab97905dcf89782
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-08 09:20:24 +0530

    added 9.js

---
+++ assignments/array_assignments/9.js
@@ -0,0 +1,13 @@
+//find th first prcie greater than 500 using break
+
+
+
+let prices =[199, 129, 98, 78, 11, 171, 888, 1055,600,768];
+
+for (let price of prices){
+    if(price>=500){
+
+        console.log(price);
+        break;
+    }
+}
