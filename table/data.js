6d1ec9a94bbe899221f0d9aba0fd5324264e36b4
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-12 09:53:35 +0530

    DDED TABLE LINKING TO JAVASCRIPT

--- table/data.js
+++ table/data.js
@@ -1,3 +1,15 @@
-let employees=[{
+let employees=[{eid:101,name:"Rahul Gandhi",esal:45000},
+    {eid:102,name:"sonia gandhi",esal:55000},
+    {eid:103,name:"priyanka gandhi",esal:65000}
     
-}]
+]
+function display_Data(){
+    let rows="";
+    for( let emp of employees){
+        rows=rows+`<tr>
+        <td>${emp.eid}</td>
+        <td>${emp.name}</td>
+        <td>${emp.esal}</td>
+        </tr>`    }
+document.getElementsByTagName('tbody')[0].innerHTML=rows;
+}
