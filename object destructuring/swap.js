89cfd8b81cdc5065c872d83d7625fdad5865ec6b
Nithin Yadav <https://github.com/Nithin-kumar009>
2026-06-23 09:24:23 +0530

    added objuect destructuring

---
+++ object destructuring/swap.js
@@ -0,0 +1,11 @@
+
+//object destructuring improves code readability
+ let state={
+    uid:101,
+    uname:"rahul",
+    loc:"wayanad"
+ }
+ let {uname,loc}=state;
+
+ console.log(uname);
+ console.log(loc);
